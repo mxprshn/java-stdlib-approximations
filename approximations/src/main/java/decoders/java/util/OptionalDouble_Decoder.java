b65a96f7829243823ffd4fcab740b4ea840c554f
alexeev509 <alexeevy509@gmail.com>
2023-11-20 21:47:18 +0300

    Optional Double plain realization;

---
+++ approximations/src/main/java/decoders/java/util/OptionalDouble_Decoder.java
@@ -0,0 +1,46 @@
+package decoders.java.util;
+
+import decoders.BaseDecoder;
+import java.util.OptionalDouble;
+import org.jacodb.api.JcClassOrInterface;
+import org.jacodb.api.JcField;
+import org.jacodb.api.JcMethod;
+import org.usvm.api.decoder.DecoderApi;
+import org.usvm.api.decoder.DecoderFor;
+import org.usvm.api.decoder.ObjectData;
+import org.usvm.api.decoder.ObjectDecoder;
+
+@DecoderFor(OptionalDouble.class)
+public class OptionalDouble_Decoder extends BaseDecoder implements ObjectDecoder {
+    @Override
+    public <T> T createInstance(final JcClassOrInterface approx,
+                                final ObjectData<T> approxData,
+                                final DecoderApi<T> decoder) {
+        T result = null;
+        {
+            JcField value = getField(approx.getDeclaredFields(), "value");
+
+            final ObjectData<T> valueData = approxData.getObjectField(value);
+            if (valueData == null) {
+                JcMethod m_empty = getMethod(approx.getDeclaredMethods(), "empty");
+
+                result = callStatic(decoder, m_empty);
+            } else {
+                JcMethod m_of = getMethod(approx.getDeclaredMethods(), "of", "java.lang.Object");
+
+                result = callStatic(decoder, m_of,
+                        approxData.decodeField(value)
+                );
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public <T> void initializeInstance(final JcClassOrInterface approx,
+                                       final ObjectData<T> approxData,
+                                       final T instance,
+                                       final DecoderApi<T> decoder) {
+        // nothing
+    }
+}
