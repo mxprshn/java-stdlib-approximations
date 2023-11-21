f99cd912e9beee3ea073a10d205d4a5dfd39b123
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2023-11-21 23:11:58 +0300

    Added decoders for `java.util.OptionalInt/Long`, `java.util.concurrent.atomic.AtomicBoolean/Integer/Long/Reference` with basic tests + Minor improvements to `Optional` decoder

---
+++ tests/src/main/java/decoders/java/util/concurrent/atomic/AtomicLong_DecoderTests.java
@@ -0,0 +1,12 @@
+package decoders.java.util.concurrent.atomic;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SuppressWarnings({"unused"})
+public final class AtomicLong_DecoderTests {
+
+    public static int test_0(final AtomicLong obj) {
+        return obj.get() == 321L ? 0 : 1;
+    }
+
+}
