00baf0f79897a25f4f11fe76d611ec48a99bfb7b
alexeev509 <alexeevy509@gmail.com>
2023-11-21 17:32:46 +0300

    OptionalDouble decoder new realization;

--- approximations/src/main/java/decoders/java/util/OptionalDouble_Decoder.java
+++ approximations/src/main/java/decoders/java/util/OptionalDouble_Decoder.java
@@ -1,7 +1,11 @@
 package decoders.java.util;
 
 import decoders.BaseDecoder;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.OptionalDouble;
+
 import org.jacodb.api.JcClassOrInterface;
 import org.jacodb.api.JcField;
 import org.jacodb.api.JcMethod;
@@ -11,35 +15,66 @@ import org.usvm.api.decoder.ObjectData;
 import org.usvm.api.decoder.ObjectDecoder;
 
 @DecoderFor(OptionalDouble.class)
-public class OptionalDouble_Decoder extends BaseDecoder implements ObjectDecoder {
+public class OptionalDouble_Decoder implements ObjectDecoder {
+    private volatile static JcField cached_OptionalDouble_value = null;
+    private volatile static JcMethod cached_OptionalDouble_of = null;
+    private volatile static Object cached_decoded_OptionalDouble_empty = null;
+
     @Override
-    public <T> T createInstance(final JcClassOrInterface approx,
-                                final ObjectData<T> approxData,
+    public <T> T createInstance(final JcClassOrInterface approximation,
+                                final ObjectData<T> approximationData,
                                 final DecoderApi<T> decoder) {
-        T result = null;
-        {
-            JcField value = getField(approx.getDeclaredFields(), "value");
-
-            final ObjectData<T> valueData = approxData.getObjectField(value);
-            if (valueData == null) {
-                JcMethod m_empty = getMethod(approx.getDeclaredMethods(), "empty");
-
-                result = callStatic(decoder, m_empty);
-            } else {
-                JcMethod m_of = getMethod(approx.getDeclaredMethods(), "of", "java.lang.Object");
-
-                result = callStatic(decoder, m_of,
-                        approxData.decodeField(value)
-                );
+        JcField f_value = cached_OptionalDouble_value;
+        // TODO: add class-based synchronization if needed
+        if (f_value == null) {
+            final List<JcField> fields = approximation.getDeclaredFields();
+            for (int i = 0, c = fields.size(); i < c; i++) {
+                JcField f = fields.get(i);
+                if ("value".equals(f.getName())) {
+                    cached_OptionalDouble_value = f_value = f;
+                    break;
+                }
             }
+
+            JcMethod Optional_empty = null;
+            JcMethod Optional_of = null;
+            final List<JcMethod> methods = approximation.getDeclaredMethods();
+            for (int i = 0, c = methods.size(); i < c; i++) {
+                JcMethod m = methods.get(i);
+
+                if (!m.isStatic()) continue;
+
+                String name = m.getName();
+                int paramCount = m.getParameters().size();
+
+                if (Optional_of == null && "of".equals(name) && paramCount == 1) {
+                    Optional_of = m;
+                    continue;
+                }
+                if (Optional_empty == null && "empty".equals(name) && paramCount == 0) {
+                    Optional_empty = m;
+                }
+
+                if (Optional_of != null && Optional_empty != null)
+                    break;
+            }
+
+            cached_OptionalDouble_of = Optional_of;
+            cached_decoded_OptionalDouble_empty = decoder.invokeMethod(Optional_empty, Collections.emptyList());
         }
-        return result;
+
+        if (approximationData.getObjectField(f_value) == null)
+            return (T) cached_decoded_OptionalDouble_empty;
+        else
+            return decoder.invokeMethod(cached_OptionalDouble_of, Collections.singletonList(
+                    approximationData.decodeField(f_value)
+            ));
     }
 
     @Override
-    public <T> void initializeInstance(final JcClassOrInterface approx,
-                                       final ObjectData<T> approxData,
-                                       final T instance,
+    public <T> void initializeInstance(final JcClassOrInterface approximation,
+                                       final ObjectData<T> approximationData,
+                                       final T outputInstance,
                                        final DecoderApi<T> decoder) {
         // nothing
     }
