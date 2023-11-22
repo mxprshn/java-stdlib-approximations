8cf966bc84f188da4d960fac1779577190ddbba0
alexeev509 <alexeevy509@gmail.com>
2023-11-22 17:05:49 +0300

    OptionalDouble fixes;

--- approximations/src/main/java/decoders/java/util/OptionalDouble_Decoder.java
+++ approximations/src/main/java/decoders/java/util/OptionalDouble_Decoder.java
@@ -15,58 +15,76 @@ import org.usvm.api.decoder.ObjectDecoder;
 @DecoderFor(OptionalDouble.class)
 public class OptionalDouble_Decoder implements ObjectDecoder {
     private volatile static JcField cached_OptionalDouble_value = null;
+    private volatile static JcField cached_OptionalDouble_present = null;
     private volatile static JcMethod cached_OptionalDouble_of = null;
     private volatile static Object cached_decoded_OptionalDouble_empty = null;
 
+    @SuppressWarnings({"unchecked", "ForLoopReplaceableByForEach"})
     @Override
     public <T> T createInstance(final JcClassOrInterface approximation,
                                 final ObjectData<T> approximationData,
                                 final DecoderApi<T> decoder) {
-        JcField f_value = cached_OptionalDouble_value;
+        JcField f_present = cached_OptionalDouble_present;
         // TODO: add class-based synchronization if needed
-        if (f_value == null) {
+        if (f_present == null) {
+            JcField f_value = null;
             final List<JcField> fields = approximation.getDeclaredFields();
             for (int i = 0, c = fields.size(); i < c; i++) {
                 JcField f = fields.get(i);
-                if ("value".equals(f.getName())) {
-                    cached_OptionalDouble_value = f_value = f;
-                    break;
-                }
-            }
+                String name = f.getName();
 
-            JcMethod OptionalDouble_empty = null;
-            JcMethod OptionalDouble_of = null;
-            final List<JcMethod> methods = approximation.getDeclaredMethods();
-            for (int i = 0, c = methods.size(); i < c; i++) {
-                JcMethod m = methods.get(i);
-
-                if (!m.isStatic()) continue;
-
-                String name = m.getName();
-                int paramCount = m.getParameters().size();
-
-                if (OptionalDouble_of == null && "of".equals(name) && paramCount == 1) {
-                    OptionalDouble_of = m;
+                if ("value".equals(name)) {
+                    f_value = f;
                     continue;
                 }
-                if (OptionalDouble_empty == null && "empty".equals(name) && paramCount == 0) {
-                    OptionalDouble_empty = m;
+                if ("present".equals(name)) {
+                    f_present = f;
                 }
 
-                if (OptionalDouble_of != null && OptionalDouble_empty != null)
+                if (f_value != null && f_present != null)
                     break;
             }
-
-            cached_OptionalDouble_of = OptionalDouble_of;
-            cached_decoded_OptionalDouble_empty = decoder.invokeMethod(OptionalDouble_empty, Collections.emptyList());
+            cached_OptionalDouble_present = f_present;
+            cached_OptionalDouble_value = f_value;
         }
 
-        if (((Double) approximationData.decodeField(f_value)) == 0.0d)
-            return (T) cached_decoded_OptionalDouble_empty;
-        else
-            return decoder.invokeMethod(cached_OptionalDouble_of, Collections.singletonList(
-                    approximationData.decodeField(f_value)
+        if (approximationData.getBooleanField(f_present)) {
+            JcMethod m_of = cached_OptionalDouble_of;
+            // TODO: add class-based synchronization if needed
+            if (m_of == null) {
+                final List<JcMethod> methods = approximation.getDeclaredMethods();
+                for (int i = 0, c = methods.size(); i < c; i++) {
+                    JcMethod m = methods.get(i);
+
+                    if (!m.isStatic()) continue;
+                    if (!"of".equals(m.getName())) continue;
+
+                    cached_OptionalDouble_of = m_of = m;
+                    break;
+                }
+            }
+
+            final double value = approximationData.getDoubleField(cached_OptionalDouble_value);
+            return decoder.invokeMethod(m_of, Collections.singletonList(
+                    decoder.createDoubleConst(value)
             ));
+        } else {
+            Object ctor = cached_decoded_OptionalDouble_empty;
+            // TODO: add class-based synchronization if needed
+            if (ctor == null) {
+                final List<JcMethod> methods = approximation.getDeclaredMethods();
+                for (int i = 0, c = methods.size(); i < c; i++) {
+                    JcMethod m = methods.get(i);
+
+                    if (!m.isStatic()) continue;
+                    if (!"empty".equals(m.getName())) continue;
+                    if (!m.getParameters().isEmpty()) continue;
+
+                    return (T) (cached_decoded_OptionalDouble_empty = decoder.invokeMethod(m, Collections.emptyList()));
+                }
+            }
+            return (T) ctor;
+        }
     }
 
     @Override
