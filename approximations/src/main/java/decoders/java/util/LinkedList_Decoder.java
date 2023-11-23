0b275930e78c6efb7be28578b4c1ab335fb99b2c
alexeev509 <alexeevy509@gmail.com>
2023-11-23 15:56:31 +0300

    LinkedHashSet and LinkedList fixes;

--- approximations/src/main/java/decoders/java/util/LinkedList_Decoder.java
+++ approximations/src/main/java/decoders/java/util/LinkedList_Decoder.java
@@ -14,43 +14,46 @@ import java.util.*;
 
 @DecoderFor(LinkedList.class)
 public class LinkedList_Decoder implements ObjectDecoder {
-
     private volatile static JcMethod cached_LinkedList_ctor = null;
-    private volatile static JcField cached_LinkedList_size = null;
     private volatile static JcField cached_LinkedList_storage = null;
-    private volatile static JcField cached_LinkedList_modCount = null;
     private volatile static JcMethod cached_LinkedList_add = null;
-    private volatile static JcMethod[] cachedMethods = null;
 
     @Override
     public <T> T createInstance(final JcClassOrInterface approximation,
                                 final ObjectData<T> approximationData,
                                 final DecoderApi<T> decoder) {
-        JcMethod ctor = cached_LinkedList_ctor;
+        JcMethod m_ctor = cached_LinkedList_ctor;
         // TODO: add class-based synchronization if needed
-        if (ctor == null) {
-            // looking for constructor
-            // TODO: add class-level synchronization if needed
-            final List<JcMethod> methodList = approximation.getDeclaredMethods();
-            final int methodCount = methodList.size();
-            JcMethod[] methods = new JcMethod[methodCount];
-            cachedMethods = methods = methodList.toArray(methods);
+        if (m_ctor == null) {
+            JcMethod m_add = null;
+            final List<JcMethod> methods = approximation.getDeclaredMethods();
+            for (int i = 0, c = methods.size(); i < c; i++) {
+                JcMethod m = methods.get(i);
 
-            for (int i = 0; i != methodCount; i++) {
-                JcMethod m = methods[i];
+                if (m.isConstructor()) {
+                    if (m_ctor == null) {
+                        if (!m.getParameters().isEmpty()) continue;
 
-                if (!m.isConstructor()) continue;
+                        cached_LinkedList_ctor = m_ctor = m;
+                    }
+                } else {
+                    if (m_add == null) {
+                        if (!"add".equals(m.getName())) continue;
 
-                List<JcParameter> params = m.getParameters();
-                if (params.size() != 0) continue;
+                        List<JcParameter> params = m.getParameters();
+                        if (params.size() != 1) continue;
+                        if (!"java.lang.Object".equals(params.get(0).getType().getTypeName())) continue;
 
-                cached_LinkedList_ctor = ctor = m;
-                break;
+                        cached_LinkedList_add = m_add = m;
+                    }
+                }
+
+                if (m_ctor != null && m_add != null)
+                    break;
             }
         }
 
-        final List<T> args = new ArrayList<>();
-        return decoder.invokeMethod(ctor, args);
+        return decoder.invokeMethod(m_ctor, (List<T>) Collections.EMPTY_LIST);
     }
 
     @Override
@@ -58,63 +61,25 @@ public class LinkedList_Decoder implements ObjectDecoder {
                                        final ObjectData<T> approximationData,
                                        final T outputInstance,
                                        final DecoderApi<T> decoder) {
-        JcField f_ls_storage = cached_LinkedList_storage;
-        JcField f_ls_size = cached_LinkedList_size;
-        JcField f_ls_modCount = cached_LinkedList_modCount;
-        if (cached_LinkedList_size == null) {
-            // TODO: add class-level synchronization if needed
+        JcField f_storage = cached_LinkedList_storage;
+        if (f_storage == null) {
+            // TODO: add class-based synchronization if needed
             final List<JcField> fields = approximation.getDeclaredFields();
-            for (int i = 0, c = fields.size(); i != c; i++) {
-                JcField field = fields.get(i);
-                String fieldName = field.getName();
-
-                if ("storage".equals(fieldName)) f_ls_storage = field;
-                else if ("size".equals(fieldName)) f_ls_size = field;
-                else if ("modCount".equals(fieldName)) f_ls_modCount = field;
-
-                // early termination
-                if (f_ls_size != null && f_ls_storage != null && f_ls_modCount != null)
+            for (int i = 0, c = fields.size(); i < c; i++) {
+                JcField f = fields.get(i);
+                if ("storage".equals(f.getName())) {
+                    cached_LinkedList_storage = f_storage = f;
                     break;
+                }
             }
-            cached_LinkedList_size = f_ls_size;
-            cached_LinkedList_storage = f_ls_storage;
-            cached_LinkedList_modCount = f_ls_modCount;
         }
 
-        // skip empty or erroneous objects
-        int size = approximationData.getIntField(f_ls_size);
-        if (size < 0)
-            throw new InternalError("Invalid container: negative size");
-        if (size == 0)
-            return;
-        final ObjectData<T> storageData = approximationData.getObjectField(f_ls_storage);
-        if (storageData == null)
-            throw new InternalError("Invalid container: storage is NULL");
-
-        // get primary method
-        JcMethod m_add = cached_LinkedList_add;
-        if (m_add == null) {
-            // TODO: add synchronization if needed
-            final JcMethod[] methods = cachedMethods;
-            for (int i = 0, c = methods.length; i != c; i++) {
-                JcMethod m = methods[i];
-
-                if (!"add".equals(m.getName())) continue;
-                List<JcParameter> params = m.getParameters();
-                if (params.size() != 1) continue;
-                if (!"java.lang.Object".equals(params.get(0).getType().getTypeName())) continue;
-
-                m_add = m;
-                break;
-            }
-            cached_LinkedList_add = m_add;
-        }
-
-        SymbolicList<T> storage = approximationData.decodeSymbolicListField(f_ls_storage);
-
-        for (int i = 0; i < storage.size(); i++) {
-            T curElement = storage.get(i);
-            decoder.invokeMethod(m_add, new LinkedList<>(Arrays.asList(outputInstance, curElement)));
+        final SymbolicList<T> storage = approximationData.decodeSymbolicListField(f_storage);
+        for (int i = 0, c = storage.size(); i < c; i++) {
+            ArrayList<T> args = new ArrayList<>();
+            args.add(outputInstance);
+            args.add(storage.get(i));
+            decoder.invokeMethod(cached_LinkedList_add, args);
         }
     }
 }
