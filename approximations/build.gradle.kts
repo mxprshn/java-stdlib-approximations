1a3e968903837ef5d3d149c8a52bba8de7da6d2c
Maksim Parshin <mxprshn@gmail.com>
2024-08-14 16:32:30 +0300

    Add junit 5 assertions approximations

--- approximations/build.gradle.kts
+++ approximations/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter-data-jpa:3.2.0")
     // Fixes "unknown enum constant 'When.MAYBE'" warning
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+    compileOnly("org.junit.jupiter:junit-jupiter:5.10.2")
 }
 
 group = "org.usvm.approximations.java.stdlib"
